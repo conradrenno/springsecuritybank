2a92b298fb87afc69e4dcdfd46f272aef71faea9
conradrenno <conradrenno@hotmail.com>
2025-03-07 18:37:13 -0300

    Configuring UserDetailsService method bean, adding InMemoryUserDetailsManager and a passwordEncoder bean

--- src/main/java/com/devrenno/springsecuritybank/config/ProjectSecurityConfig.java
+++ src/main/java/com/devrenno/springsecuritybank/config/ProjectSecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,4 +27,19 @@ public class ProjectSecurityConfig {
         http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user = User.withUsername("user").password(passwordEncoder().encode("12345")).authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password(passwordEncoder().encode("123456")).authorities("admin").build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
